9fe39be3c3d594beb4fd21b190514d17b703b65c
Ak5092 <akshatsaraswat00@gmail.com>
2026-03-24 01:51:53 -0400

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import LacunaTrade from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LacunaTrade />
-  </React.StrictMode>,
-)
